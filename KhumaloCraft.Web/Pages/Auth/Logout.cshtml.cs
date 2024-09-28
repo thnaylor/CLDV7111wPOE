ef97b4454ef3e18921c2620100690118ae70069f
tnaylor <thomasnaylor@outlook.com>
2024-09-28 21:53:07 +0200

    #39 - fix logout bug

--- KhumaloCraft.Web/Pages/Auth/Logout.cshtml.cs
+++ KhumaloCraft.Web/Pages/Auth/Logout.cshtml.cs
@@ -3,16 +3,14 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace KhumaloCraft.Web.Pages.Auth
+namespace KhumaloCraft.Pages.Auth
 {
   public class LogoutModel : PageModel
   {
-    public async Task<IActionResult> OnPostLogoutAsync()
+    public async Task<IActionResult> OnPostAsync()
     {
-      // Sign out the user
-      await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+      Response.Cookies.Delete("AuthToken");
 
-      // Redirect to the home page after logout
       return RedirectToPage("/Index");
     }
   }
